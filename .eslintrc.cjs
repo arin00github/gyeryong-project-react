1f53dbb946ec40f10d268d1249c2d80f4531ade2
otter0104 <otter0104@innodep.com>
2023-10-27 12:31:54 +0900

    🔧 Config: eslint, prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parseOption: {
+    sourceType: 'module',
+    project: ["./tsconfig.json"]
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs', "vite.config.ts"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import', 'prettier', "@typescript-eslint", "react-hooks", ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "import/order": [
+      "error",
+      {
+          alphabetize: {
+              order: "asc",
+              caseInsensitive: true,
+          },
+          "newlines-between": "always",
+          groups: ["builtin", "external", "parent", "sibling", "index"],
+          pathGroups: [
+              {
+                  pattern: "react",
+                  group: "external",
+                  position: "before",
+              },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+      },
+  ],
+  "prettier/prettier": [
+      "error",
+      {
+          endOfLine: "auto",
+      },
+  ],
+  "no-unused-vars": "off",
+  },
+}
